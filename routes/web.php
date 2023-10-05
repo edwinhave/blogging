8339a39d7c36e3f7599cc6e11b4a74a46698adc1
Edwin Jhon <bluecookies.var@gmail.com>
2023-10-05 19:28:15 +0700

    create data

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\LandingController;
 use Illuminate\Support\Facades\Route;
@@ -23,3 +24,5 @@ Route::get('/about', [AboutController::class, 'index']); //->name('about');
 Route::get('/contact-us', [ContactController::class, 'index'])->name('contact-us.index');
 
 Route::post('contact-us', [ContactController::class, 'store'])->name('contact-us.store');
+
+Route::resource('articles', ArticleController::class);
